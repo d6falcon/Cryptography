88eb5b05e7143655bb7d6239a7d82403369946fe
Srikanth Dabbiru <9648746+d6falcon@users.noreply.github.com>
2024-02-06 12:49:32 +0000

    Substitution cipher

---
+++ substitutionCipher.py
@@ -0,0 +1,30 @@
+#This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation
+#Author: Srikanth Dabbiru
+#Implement a substitution cipher (random key) taking the message as the input
+import random
+
+def keygen(x):
+    alphabets = "abcdefghijklmnopqrstuvwxyz"
+    Alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    calpha = list(alphabets)
+    cAlpha = list(Alphabets)
+    key = {}
+    for char in alphabets:
+        key[char] = calpha.pop(random.randint(0, len(calpha) - 1)) 
+    for char in Alphabets:
+        key[char] = calpha.pop(random.randint(0, len(cAlpha) - 1)) 
+    return key
+
+def encrypt(cipher, plaintext):
+    encrypted = ""
+    for i in plaintext:
+       if i in cipher:
+        encrypted += cipher[i]
+       else:    #Keep unknown chars from keygen as-is
+        encrypted += i 
+    return encrypted
+
+cipher = keygen() # Random char
+plaintext = input("Enter your message to be encrypted: ")
+enc_message = encrypt(cipher, plaintext)
+print("Here is your encrypted message: " + enc_message)
