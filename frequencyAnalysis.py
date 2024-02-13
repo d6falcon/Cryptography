c845981d773c32b10e7a0228161f1494e18593f1
Srikanth Dabbiru <9648746+d6falcon@users.noreply.github.com>
2024-02-13 11:39:15 +0000

    Updated frequency analysis constructor class

--- frequencyAnalysis.py
+++ frequencyAnalysis.py
@@ -14,9 +14,40 @@ bpmb bpr xjhhjcwko wi bpr sujsru msshwvmbwjk mkd
 wkbrusurbmbwjk w jxxru yt bprjuwri wk bpr pjsr bpmb bpr
 riirkvr jx jqwkmcmk qmumbr cwhh urymwk wkbmvb"""
 
+
+
 # source of frequency can be found in readme.md
-alphabet_frequency = {'a': 0.0817, 'b': 0.0150, 'c': 0.0278, 'd': 0.0425, 'e': 0.1270, 'f': 0.0223,
-               'g': 0.0202, 'h': 0.0609, 'i': 0.0697, 'j': 0.0015, 'k': 0.0077, 'l': 0.0403,
-               'm': 0.0241, 'n': 0.0675, 'o': 0.0751, 'p': 0.0193, 'q': 0.0010, 'r': 0.0599,
-               's': 0.0633, 't': 0.0906, 'u': 0.0276, 'v': 0.0098, 'w': 0.0236, 'x': 0.0015,
-               'y': 0.0197, 'z': 0.0007}
+# alphabets_frequency = {'a': 0.0817, 'b': 0.0150, 'c': 0.0278, 'd': 0.0425, 'e': 0.1270, 'f': 0.0223,
+#                'g': 0.0202, 'h': 0.0609, 'i': 0.0697, 'j': 0.0015, 'k': 0.0077, 'l': 0.0403,
+#                'm': 0.0241, 'n': 0.0675, 'o': 0.0751, 'p': 0.0193, 'q': 0.0010, 'r': 0.0599,
+#                's': 0.0633, 't': 0.0906, 'u': 0.0276, 'v': 0.0098, 'w': 0.0236, 'x': 0.0015,
+#                'y': 0.0197, 'z': 0.0007}
+class Analysis:
+  def __init__(self): # constructor class
+    self.alphabets = "abcdefghijklmnopqrstuvwxyz"
+    self.frequency = {}
+    
+  def calculate_frequency(self, ciphertext): # calculate frequency method
+    for char in self.alphabets:
+      self.frequency[char] = 0
+  
+    textlength = 0
+    for alphakey in ciphertext:
+      if alphakey in self.frequency:
+        self.frequency[alphakey] += 1
+        textlength += 1
+
+    for char in self.frequency:
+      self.frequency[char] = round(self.frequency[char]/textlength, 4)
+
+  def print_frequency(self): # print frequency
+    line_count = 0
+    for char in self.frequency:
+      print(char, ':', self.frequency[char], ' ', end ='')
+      if line_count % 3 == 2:
+        print()
+      line_count += 1
+      
+analyse = Analysis()
+analyse.calculate_frequency(ciphertext)
+analyse.print_frequency()
