caa9c3a7854caa35ea091a4b9126321d0a6aaef3
Srikanth Dabbiru <9648746+d6falcon@users.noreply.github.com>
2024-04-05 07:37:08 +0100

    Updated Stream Cipher

--- streamCipher.py
+++ streamCipher.py
@@ -3,7 +3,7 @@
 #Implements a stream cipher. It is similar to a one time pad except the number is randomly generated. For random generation, we use something called LCG (linear congruential generator) - generate random keys but each time the function is run it generates the same set of random keys thus solving the problem that one time pads had
 
 
-class keyStream:    #define a class with key stream needed, default the seed by 1 if not called at line 17
+class keyStream:    #define a class with key stream needed, default the seed by 1 if not called at line 20
     def __init__(self, key=1):
         self.next = key
         
@@ -13,19 +13,28 @@ class keyStream:    #define a class with key stream needed, default the seed by 
     
     def generate_keystream(self):
         return self.random() % 1024  # returns random numbers between 0 to 1024
+  
+  
+def encrypt(key, message):
+    return bytes([message[i] ^ key.generate_keystream() for i in range(len(message))])
     
-key = keyStream(15) # insert any seed value here to generate same set of random numbers
+cipherkey = keyStream(9) # insert any seed value here to generate same set of random numbers
 for i in range(10):
-    print(key.generate_keystream())
-    
-# work in progress...
+    print(cipherkey.generate_keystream())
 
-# declare a message
-# encrypt
-# decrypt
+cipherkey = keyStream(15)
+message = "Cryptography is the key".encode()
 
+#Encryption
+ciphertext = encrypt(cipherkey, message)
+print(ciphertext)
+
+#Decryption
+deciphertext = encrypt(cipherkey, ciphertext)
+print(deciphertext)
 
 # explain problem(s) with stream cipher:
    # No Authenticity - intercepting and changing the original message
    # Key reuse
    # Low entropy
+   
